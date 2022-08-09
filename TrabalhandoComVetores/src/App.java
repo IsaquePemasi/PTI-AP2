09c4328c27187cfc1a87f19b3f16b93da0c746f8
IsaqueZaratustra <ADSisaque@gmail.com>
2022-08-09 17:31:50 -0300

    add

--- TrabalhandoComVetores/src/App.java
+++ TrabalhandoComVetores/src/App.java
@@ -8,15 +8,17 @@ public class App {
         Random random = new Random();
 //ARRAY_LIST
         ArrayList <Integer> dados = new ArrayList<>();
-        for (int i = 0; i <10; i++) {
-            dados.add(random.nextInt(1000));
+        for (int i = 0; i <100; i++) {
+            dados.add(random.nextInt(9999));
         }
 //INICIALIZAÇÃO
         System.out.println("Qual é a quantidade de elementos que serão lidos, no vetor?");
         String quantidade = scanner.nextLine();
 
+        for(int i=0; i<quantidade.length(); i++){
             System.out.println(dados);
-      
+        }
+            //System.out.println(dados);
 
     }
 }
