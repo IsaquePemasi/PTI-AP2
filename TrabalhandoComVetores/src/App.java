4d4f771c2477cb248b9f499fd03bebeaefc9294e
IsaqueZaratustra <ADSisaque@gmail.com>
2022-08-09 17:03:13 -0300

    add

--- TrabalhandoComVetores/src/App.java
+++ TrabalhandoComVetores/src/App.java
@@ -1,15 +1,22 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Random;
 
 public class App {
     public static void main(String[] args) throws Exception {
-
         Scanner scanner = new Scanner(System.in);
-
-        sytem.out.println("Qual é a quantidade de elementos que serão lidos, no vetor");
-        String QuatidadeVetor = scanner.nextLine();
-
-        for (i = 0; i < QuantidadeVetor; i++) {
-            
+        Random random = new Random();
+//ARRAY_LIST
+        ArrayList <Integer> dados = new ArrayList<>();
+        for (int i = 0; i <10; i++) {
+            dados.add(random.nextInt(1000));
         }
+//INICIALIZAÇÃO
+        System.out.println("Qual é a quantidade de elementos que serão lidos, no vetor?");
+        String quantidade = scanner.nextLine();
+
+            System.out.println(dados);
+      
+
     }
 }
