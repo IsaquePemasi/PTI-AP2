8bdd8aedd7c4dc983e2479a77b2422b3cfede220
IsaqueZaratustra <ADSisaque@gmail.com>
2022-08-10 13:38:04 -0300

    add

--- TrabalhandoComVetores/src/App.java
+++ TrabalhandoComVetores/src/App.java
@@ -3,26 +3,29 @@ import java.util.*;
 class Exercicio {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("-------------------------------------------");
         System.out.println("Insira o tamanho do vetor: ");
         int n = scanner.nextInt();
-        System.out.println("Este é o tamanho do vetor escolhido: " + n);
+        System.out.println("Tamanho do vetor escolhido: " + n);
+        System.out.println("-------------------------------------------");
         System.out.println("Agora, insira os elementos do seu vetor: ");
         ArrayList<Integer> vetor = new ArrayList<>();
         for(int i = 0; i < n; i++) {
             vetor.add(scanner.nextInt());
         }
-        System.out.println("Os " + vetor.size() + " valores informados são: ");
+        System.out.println("Os " + vetor.size() + " valores informados sao: ");
         for(int i = 0; i < n; i++) {
             System.out.println(vetor.get(i));
         }
-    
+        System.out.println("-------------------------------------------");
         if(ordenadoCrescente(vetor)) {
-            System.out.println("Os valores informados, estão ordenados!");
+            System.out.println("Os valores informados, estao ordenados!");
         } else {
-            System.out.println("Os valores informados, Nao estão ordenados!");
+            System.out.println("Os valores informados, nao estao ordenados!");
         }
+        System.out.println("-------------------------------------------");
         int resultado = maiorDiferenca(vetor);
-        System.out.println("A maior diferença entre os elementos é: " + resultado);
+        System.out.println("A maior diferenca entre os elementos: " + resultado);
     }
     
     public static int maiorDiferenca(ArrayList<Integer> vetor) {
