c55170310bf46562d73dfd7f60142a55397b2be6
IsaqueZaratustra <ADSisaque@gmail.com>
2022-08-10 12:56:24 -0300

    add

--- TrabalhandoComVetores/src/App.java
+++ TrabalhandoComVetores/src/App.java
@@ -1,24 +1,42 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.Random;
+import java.util.*;
 
-public class App {
-    public static void main(String[] args) throws Exception {
+class Exercicio {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
-//ARRAY_LIST
-        ArrayList <Integer> dados = new ArrayList<>();
-        for (int i = 0; i <100; i++) {
-            dados.add(random.nextInt(9999));
+        System.out.println("Insira o tamanho do vetor: ");
+        int n = scanner.nextInt();
+        System.out.println("Este é o tamanho do vetor escolhido: " + n);
+        System.out.println("Agora, insira os elementos do seu vetor: ");
+        ArrayList<Integer> vetor = new ArrayList<>();
+        for(int i = 0; i < n; i++) {
+            vetor.add(scanner.nextInt());
         }
-//INICIALIZAÇÃO
-        System.out.println("Qual é a quantidade de elementos que serão lidos, no vetor?");
-        String quantidade = scanner.nextLine();
-
-        for(int i=0; i<quantidade.length(); i++){
-            System.out.println(dados);
+        System.out.println("Os " + vetor.size() + " valores informados são: ");
+        for(int i = 0; i < n; i++) {
+            System.out.println(vetor.get(i));
         }
-            //System.out.println(dados);
-
+    
+        if(ordenadoCrescente(vetor)) {
+            System.out.println("Os valores informados, estão ordenados!");
+        } else {
+            System.out.println("Os valores informados, Nao estão ordenados!");
+        }
+        int resultado = maiorDiferenca(vetor);
+        System.out.println("A maior diferença entre os elementos é: " + resultado);
+    }
+    
+    public static int maiorDiferenca(ArrayList<Integer> vetor) {
+        Collections.sort(vetor);
+        int resultado = vetor.get(vetor.size() - 1) - vetor.get(0);
+        return resultado;
+    }
+    
+    public static boolean ordenadoCrescente(ArrayList<Integer> vetor) {
+        for(int i = 0; i < vetor.size() - 1; i++) {
+            if(vetor.get(i) > vetor.get(i+1)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
